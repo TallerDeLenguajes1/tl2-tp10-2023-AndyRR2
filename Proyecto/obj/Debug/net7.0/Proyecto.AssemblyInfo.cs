ae1abf37eb79b8bd6ee956e75f40797cdd055847
Andy Rodriguez <andyarr222@gmail.com>
2024-02-16 00:30:24 -0300

    cambio de carpetas principales

--- Proyecto/obj/Debug/net7.0/Proyecto.AssemblyInfo.cs
+++ Proyecto/obj/Debug/net7.0/Proyecto.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Tp11")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Tp11")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Tp11")]
+[assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
